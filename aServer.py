04cd7c2a89f538b8e1b93973b0367259367f0278
Pritesh Agarwal <pa5174@srmist.edu.in>
2024-01-13 19:02:03 +0530

    added fpv switch support

--- aServer.py
+++ aServer.py
@@ -4,40 +4,34 @@ import json
 import can
 import serial
 import pickle
+import time
 from canSR import build, disect
 
-cid={'idmo':200,'bio':300}
-interface = sys.argv[1]
-can_id = cid[sys.argv[2]]
-#bus = can.Bus(interface = 'socketcan',channel = interface, receive_own_messages = True)
-sob=serial.Serial(
-		port='/dev/ttyACM0',
-		baudrate=115200,
-		parity=serial.PARITY_NONE,
-		stopbits=serial.STOPBITS_ONE,
-		bytesize=serial.EIGHTBITS,
-		timeout=1
-		)      
-sob.write(bytes('S4\r\n','utf-8'))
-sob.write(bytes('O\r\n','utf-8'))
+fpv=0
 def sendCan(data): 
     data = pickle.loads(data)
     #print(data)
     pwm=list(map(int,[data["pwm1"],data["pwm2"],data["pwm3"],data["pwm4"]]))
     button=list(map(int,[data['b1'],data['b2'],data['b3'],data['b4'],data['b5']]))
     motors=[10,11,12,13]
-    mot=[24,25,26,27,28]
+    mot=[25,26,27,28]
+    
+    if button[4]==1:
+        global fpv
+        fpvmsg = build(500,0,20,fpv%4)
+        fpv+=1
+        print(fpvmsg)
+        sob.write(fpvmsg)
+
     for i in range(4):
         msg=build(can_id,pwm[i],10,motors[i])
-        print(msg)
-        #bus.send(msg)
-        sob.write(msg)
+        #print(msg)
+        #sob.write(msg)
 
-    for i in range(5):
+    for i in range(4):
         msg=build(can_id,button[i],10,mot[i])
         #print(msg)
-        #bus.send(msg)
-        sob.write(msg)
+        #sob.write(msg)
 
 
 async def handle_client(reader, writer):
@@ -93,5 +87,23 @@ async def main():
         await server.serve_forever()
 
 if __name__ == '__main__':
+    cid={'idmo':200,'bio':300}
+    interface = sys.argv[1]
+    can_id = cid[sys.argv[2]]
+    #bus = can.Bus(interface = 'socketcan',channel = interface, receive_own_messages = True)
+    sob=serial.Serial(
+        port='/dev/ttyACM'+str(interface),
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+        )
+    sob.write(bytes('h\r\n','utf-8'))
+    time.sleep(1)
+    sob.write(bytes('S4\r\n','utf-8'))
+    time.sleep(1)
+    sob.write(bytes('O\r\n','utf-8'))
+    
     asyncio.run(main())
 
